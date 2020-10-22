e266e64976bdfc053599efd6fef3cdd3f9b6b5af
lesogorovalex <galanthusan@gmail.com>
2020-10-22 16:41:50 +0300

    Updates

---
+++ precache-manifest.89284bbefb94553b54d104161572b24c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b83e27e429a2a3b27a048c2fe541bdd",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "178fdc69e118d0f4951a",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.d2c7b570.chunk.css"
+  },
+  {
+    "revision": "0b63760391d11de4bb4a",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.9a11d3b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.9a11d3b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178fdc69e118d0f4951a",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.a9d56557.chunk.js"
+  },
+  {
+    "revision": "27e9f0ab8d32b26f73ad",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.ed3ca0d9.js"
+  }
+]);
